e28d4e31ff5169af6f1750d57896979d1ca58b21
Matyáš Hýbl <hyma.code@gmail.com>
2022-12-06 12:11:43 +0100

    today also copies a template main to the directory for the day

---
+++ C/template.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE *read_file(char *filename)
+{
+    FILE *input = fopen(filename, "r");
+    if (!input)
+    {
+        printf("Error reading input file\n");
+        exit(-1);
+    }
+    return input;
+}
+
+
+void part_one(FILE *input)
+{
+    // printf("Part 1: %", );
+}
+
+void part_two(FILE *input)
+{
+    // printf("Part 2: %", );
+}
+
+int main(void)
+{
+    FILE *input;
+    input = read_file("test.txt");
+    part_one(input);
+
+    rewind(input);
+    part_two(input);
+}
--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ let
     year=$(pwd | sed -E 's|^.*aoc(....)/.+$|\1|')
      day=$(pwd | sed -E 's|.+/day(.+)|\1|g')
     curl -H "Cookie: session=$session" "https://adventofcode.com/$year/day/$day/input" > input.txt
+    cp ../../template.c ./main.c
   '';
 in
 pkgs.mkShell {
