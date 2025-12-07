de2dd214f9ab14c4ec77d2649bf8496e79acce14
Matyas Hybl <hyma.code@gmail.com>
2025-12-07 13:03:35 +0100

    move range merging into lib

---
+++ Rust/aoc/src/lib.rs
@@ -0,0 +1,24 @@
+use std::ops::Range;
+
+pub fn ranges_merge_overlapping<T: Ord + Copy>(ranges: &mut [Range<T>]) -> Vec<Range<T>> {
+    let mut ranges_nonoverlapping = Vec::new();
+    ranges.sort_by_key(|r| r.start);
+    ranges_merge_consecutive_into(ranges, &mut ranges_nonoverlapping);
+    ranges_nonoverlapping
+}
+
+pub fn ranges_merge_consecutive_into<T: Ord + Copy>(ranges: &[Range<T>], out: &mut Vec<Range<T>>) {
+    let Range { mut start, mut end } = ranges[0];
+    for range in &ranges[1..] {
+        if range.start <= end {
+            if range.end > end {
+                end = range.end;
+            }
+        } else {
+            out.push(start..end);
+            start = range.start;
+            end = range.end;
+        }
+    }
+    out.push(start..end);
+}
